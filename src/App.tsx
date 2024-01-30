36b23fe7596c4a620c79cdf556b1a79aba7a91ba
Victor Caio <kaioaraujo426@gmail.com>
2024-01-29 23:19:46 -0300

    Implementation of the default theme and changed the variable that starts the application in development mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>E aí, fala tu</h1>
-    </>
+    </ThemeProvider>
   );
 }
