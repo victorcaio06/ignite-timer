5756eb2bb31ec019bb4bd3d568d84af0fe0feb35
Victor Caio <kaioaraujo426@gmail.com>
2024-01-31 12:11:28 -0300

    FEAT: defined the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
+import { Router } from './Router';
+import { BrowserRouter } from 'react-router-dom';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>E aí, fala tu</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,8 +8,19 @@ css
     box-sizing: border-box;
   }
 
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+  }
+
   body {
-    background: #333;
-    color: #fff;
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
+  }
+  
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
   }
 `;
