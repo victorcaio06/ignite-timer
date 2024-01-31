c44c334a110e51219d3d100593edea5138f22f04
Victor Caio <kaioaraujo426@gmail.com>
2024-01-30 23:55:54 -0300

    FEAT: added global style and type to default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>E aí, fala tu</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
